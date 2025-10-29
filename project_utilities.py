4b5ad0a16f42a04805605b5c201b5f827a74d6e9
Tom Desktop <tcutler96@hotmail.com>
2025-10-29 20:19:27 +0000

    renamed

--- project_utilities.py
+++ project_utilities.py
@@ -8,7 +8,7 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = 'True'
 
 def create_project_summary():
     print('creating project summary...')
-    os.system('pygount --format=summary --suffix=py,json,mp3,wav --duplicates --folders-to-skip=temp,.* --names-to-skip=summary.txt --out=summary.txt')
+    os.system('pygount --format=summary --suffix=py,json,mp3,wav --duplicates --folders-to-skip=temp,.* --names-to-skip=project_summary.txt --out=project_summary.txt')
     with open('summary.txt', 'r', encoding='utf8') as file:
         data = file.read()
     print(data)
@@ -45,4 +45,4 @@ def clean_directory(game_name):
 
 if __name__ == '__main__':
     create_project_summary()
-    create_executable()
+    # create_executable()
